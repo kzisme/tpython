7baaa726d51134e2b1f949ec8f6586478d658bf0
Kevin Zita <bleedgreenandgold@gmail.com>
2015-12-04 01:45:43 -0500

    Adding some exercises

---
+++ chapter06/exercise.py
@@ -0,0 +1,19 @@
+def day_num(day_name):
+    if day_name == 0:
+        return "Sunday"
+    elif day_name == 1:
+        return "Monday"
+    elif day_name == 2:
+        return "Tuesday"
+    elif day_name == 3:
+        return "Wednesday"
+    elif day_name == 4:
+        return "Thursday"
+    elif day_name == 5:
+        return "Friday"
+    elif day_name == 6:
+        return "Saturday"
+
+#I'm not sure how to test a fruitful function unless you print the value...
+day_num(4)
+
---
+++ chapter06/exercise1.py
@@ -0,0 +1,5 @@
+weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+def day_name(day):
+	return weekdays[day]
+
+day_name(4)
